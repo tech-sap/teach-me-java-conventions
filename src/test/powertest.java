19c5042c6c5d9af334cddbff3cbe83cbb43562bc
SaravanaKumaran Manickam <75821663+tech-sap@users.noreply.github.com>
2022-03-06 21:37:58 +0530

    changed "powerfinder.OF()" to "PowerFinder.of()"

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -6,21 +6,21 @@ import org.junit.test;
 public class Powertest {
     @Test
     public void oneRaisedToOneIsOnew() {
-        assert powerfinder.OF(1,1) == 1;
+        assert PowerFinder.of(1,1) == 1;
     }
 
     @Test
     public void twoRaisedToOneIsTwo() {
-        assert powerfinder.OF(2,1) == 2;
+        assert PowerFinder.of(2,1) == 2;
     }
 
     @Test
     public void twoPowerTwoIsFour() {
-        assert powerfinder.OF(2, 2) == 4;
+        assert PowerFinder.of(2, 2) == 4;
     }
 
     @Test
     public void threePowerTwoIsNine() {
-        assert powerfinder.OF(3, 2) == 3*3;
+        assert PowerFinder.of(3, 2) == 3*3;
     }
 }
