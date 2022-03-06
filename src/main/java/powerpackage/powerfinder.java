ed2d1116b6a62219ef12f2c4441d7e495b845736
SaravanaKumaran Manickam <75821663+tech-sap@users.noreply.github.com>
2022-03-06 21:40:20 +0530

    changed class name by following naming conventions

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,6 +1,6 @@
 package powerpackage;
 
-public class powerfinder {
+public class PowerFinder {
     public static int of(int xyz, int pqr) {
         int p = 1;
         for (int i = 0; i<pqr; i++) {
