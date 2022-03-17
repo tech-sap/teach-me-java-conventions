90530f3074546bf5bbcc553365e8797d9c21d872
SaravanaKumaran Manickam <75821663+tech-sap@users.noreply.github.com>
2022-03-17 09:58:30 +0530

    Changed of() to power()

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -6,21 +6,21 @@ import org.junit.test;
 public class Powertest {
     @Test
     public void oneRaisedToOneIsOnew() {
-        assert PowerFinder.of(1,1) == 1;
+        assert PowerFinder.power(1,1) == 1;
     }
 
     @Test
     public void twoRaisedToOneIsTwo() {
-        assert PowerFinder.of(2,1) == 2;
+        assert PowerFinder.power(2,1) == 2;
     }
 
     @Test
     public void twoPowerTwoIsFour() {
-        assert PowerFinder.of(2, 2) == 4;
+        assert PowerFinder.power(2, 2) == 4;
     }
 
     @Test
     public void threePowerTwoIsNine() {
-        assert PowerFinder.of(3, 2) == 3*3;
+        assert PowerFinder.power(3, 2) == 3*3;
     }
 }
